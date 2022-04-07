0a39f958c53d03ee459b09356205abe52af99cb7
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2022-04-07 19:03:12 +0300

    added dropdown to chats

--- src/Pages/homepage/MsgInput.js
+++ src/Pages/homepage/MsgInput.js
@@ -3,9 +3,16 @@ import React from 'react';
 const Msginput = () => {
     return (
         <div className="wrap-message">
-            <button className='chat-icons'>
-                <i class="bi bi-paperclip"></i>
-            </button>
+        
+            <div class="btn-group dropup">
+                <button className="chat-icons dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="bi bi-paperclip"></i>
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
             <div className="message">
                 <input type="text" className="input-message" placeholder="Write a message here"></input>
             </div>
