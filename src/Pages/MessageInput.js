aa00d334e9fcf1937271f2ffecf39ddc19730f25
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2022-04-10 20:33:40 +0300

    stop recording button fix

--- src/Pages/MessageInput.js
+++ src/Pages/MessageInput.js
@@ -90,7 +90,7 @@ export default function MessageInput(props) {
 
                 {/* Set title as link to the icon (link from https://icons.getbootstrap.com/#usage) */}
                 {
-                    !existingRecord &&
+                    !existingRecord && !isLive &&
                     <DropdownButton
                         id={'dropdown-button-drop-up'}
                         drop='up'
@@ -111,7 +111,7 @@ export default function MessageInput(props) {
                         </Dropdown.Item>
                         <Dropdown.Item eventKey="2" align="center" >
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera-video-fill" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z" />
+                                <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z" />
                             </svg>
 
 
@@ -124,6 +124,16 @@ export default function MessageInput(props) {
 
                         </Dropdown.Item>
                     </DropdownButton>
+
+                }
+                {
+                    isLive && <Button className='pause-btn' id='pause-btn' variant='danger' onClick={() => {
+                        handleRecord();
+                    }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-stop-fill" viewBox="0 0 16 16">
+                            <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z" />
+                        </svg>
+                    </Button>
                 }
                 {
                     existingRecord &&
@@ -135,33 +145,36 @@ export default function MessageInput(props) {
                 }
 
                 {
-                    existingRecord && 
+                    existingRecord &&
                     <div className='recorderForm'>
                         <audio className='recorderForm col align-self-stretch' src={audioURL} controls id='player'>
                         </audio>
                     </div>
                 }
-                 {
+                {
                     !existingRecord && isLive &&
                     <div className='recorderForm'>
-                        <img className='recorderForm col align-self-stretch' src={wave}  id='gif'>
+                        <img className='recorderForm col align-self-stretch' src={wave} id='gif'>
                         </img>
                     </div>
                 }
                 {
-                    !existingRecord &&  !isLive &&
+                    !existingRecord && !isLive &&
                     <div className='messageForm' >
-                        <FormControl className='col align-self-stretch' 
-                        placeholder={isLive? "Recording in progress" :'Enter message here'}
-                        disabled={isLive? 'true':'false'} id='messageIn' required />
+                        <FormControl className='col align-self-stretch'
+                            placeholder={isLive ? "Recording in progress" : 'Enter message here'}
+                            id='messageIn' required />
                     </div>
                 }
+                {
+                    !isLive &&
+                    <Button type='submit' onClick={handleSubmit} >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                        </svg>
+                    </Button>
+                }
 
-                <Button type='submit' onClick={handleSubmit} >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-                    </svg>
-                </Button>
             </InputGroup>
         </div>
     )
