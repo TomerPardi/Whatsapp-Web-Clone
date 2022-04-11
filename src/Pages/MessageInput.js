bf6a32bdda9f7616d589fb90d2c4f308294f563c
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2022-04-11 22:59:14 +0300

    styled the message input buttons

--- src/Pages/MessageInput.js
+++ src/Pages/MessageInput.js
@@ -16,6 +16,8 @@ export default function MessageInput(props) {
     const [existingRecord, setRecorded] = useState(false);
     const [changed, setChanged] = useState(false);
     const [isLive, setLive] = useState(false);
+    const [isShown, setIsShown] = useState(false);
+
     let context = useContext(AppContext)
     //TODO : dynamically get contact in question
     const user = context.currentUser;
@@ -95,7 +97,7 @@ export default function MessageInput(props) {
                         id={'dropdown-button-drop-up'}
                         drop='up'
                         size="sm"
-                        variant={isRecording ? 'danger' : 'primary'}
+                        variant={isRecording ? 'danger' : 'light'}
                         title={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-paperclip" viewBox="0 0 16 16">
                             <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z" />
                         </svg>}
@@ -168,10 +170,15 @@ export default function MessageInput(props) {
                 }
                 {
                     !isLive &&
-                    <Button type='submit' onClick={handleSubmit} >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-                        </svg>
+                    <Button variant='light' type='submit' onClick={handleSubmit} onMouseEnter={() => setIsShown(true)}
+                        onMouseLeave={() => setIsShown(false)} >
+                            {isShown ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-check-fill" viewBox="0 0 16 16">
+                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 1.59 2.498C8 14 8 13 8 12.5a4.5 4.5 0 0 1 5.026-4.47L15.964.686Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+                                <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
+                            </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+                                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                            </svg>}
+
                     </Button>
                 }
 
