24c73e0fb6b961e86a70e634475707de3e874e7f
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2022-04-13 19:04:51 +0300

    added sorting of contact by the time of the last message

--- src/Pages/homepage/Homepage.js
+++ src/Pages/homepage/Homepage.js
@@ -12,18 +12,53 @@ const handleTabClosing = () => {
     AppContext.currentUser = 'none';
 }
 
+// compare 2 arrays of messages by the value of time in the last of them
+function compareContacts(a, b) {
+    // get the last message in the list of messages, and gets its time field.
+    let last1 = a[a.length - 1].time
+    console.log(last1)
+    let last2 = b[b.length - 1].time
+    if (last1 < last2) return 1;
+    if (last1 > last2) return -1;
+    else return 0;
+}
+
 
 
 export default function Homepage(props) {
     let context = React.useContext(AppContext)
     const [user, setUser] = useState(context.currentUser)
-    //TODO: apply change in array to context
     const [messages, setMessages] = useState(context.userData[user].contacts[context.ActiveContact])
     // a state change to trigger a re-render of the page
     const [changed, setChanged] = useState(false)
     const [active, setActive] = useState(context.ActiveContact)
+
+    function getKeyByValue(object, value) {
+        return Object.keys(object).find(key => object[key] === value);
+      }
+    
+    // function to order the contacts in the contact list and apply the value to the context
+    function orderContacts() {
+        let unordered = context.userData[user].contacts // contacts unordered
+        let ordered = Object.values(unordered).sort(compareContacts).reduce(
+            // obj is the object reduce works on - reduce iterates over the sorted array of values (the values are contacts)
+            // each time reduce adds a contact to the obj, using the value from the previous unsorted array.
+            (obj, value) => {
+                // get the key for the value in new order from unordered object
+                let key = getKeyByValue(unordered,value)
+                // set the value(array of messages) to be the value from the unsorted array, for the key, the key being the contacts name
+                obj[key] = value;
+                return obj;
+            }, 
+            {} //initial value passed to reduce - empty object
+        )
+        // save the new ordered by last message time contact list to context
+        context.userData[user].contacts = ordered;
+    }
+
     useEffect(() => {
         setChanged(false);
+        orderContacts();
         setUser(context.currentUser)
         setMessages(context.userData[user].contacts[context.ActiveContact])
         window.addEventListener('unload', handleTabClosing)
@@ -32,7 +67,6 @@ export default function Homepage(props) {
         }
     });
     function conditionalRight() {
-        // console.log(active)
         if (active === 'none') {
             return (
                 <div style={{ height: '100%', background: '#99eda1' }}>
@@ -55,7 +89,9 @@ export default function Homepage(props) {
 
     };
 
+    orderContacts();
     return (
+
         <>
             <div className="wrap">
                 <section className="left">
