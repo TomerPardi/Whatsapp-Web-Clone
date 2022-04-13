04559ca77824aa44ba7cdd03375a1f8e4da0fccb
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2022-04-13 15:57:11 +0300

    added fixed to larger image modal view

--- src/Pages/homepage/ChatBubble.js
+++ src/Pages/homepage/ChatBubble.js
@@ -1,29 +1,31 @@
 import React from 'react'
-import PhotoViewModal from './PhotoViewModal';
-import { render } from '@testing-library/react';
+import { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap'
 
 
 export default function ChatBubble(props) {
     const { text, time, isSelf, type, photo, audio } = props.message;
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    function handleShowFull(photo){
-        render(
-            <PhotoViewModal show={true} photo={photo}/>
-        )
-    }
 
 
     if (type === 'photo') {
         return (
             <div
                 className={`chat-bubble ${isSelf ? "me" : "you"} `}>
-                <img src={(photo.includes('64') || photo.includes('blob'))? photo : require(`${photo}`)}
-                style={{ maxWidth: '200px' }}
-                onClick= {()=>{handleShowFull(photo)}}
-                />
-                <br>
-                </br>
+                {/* <img src={(photo.includes('64') || photo.includes('blob')) ? photo : require(`${photo}`)} style={{ maxWidth: '200px' }} /> */}
+                <Button variant="link" className=' shadow-none' onClick={handleShow}>
+                    <img src={(photo.includes('64') || photo.includes('blob')) ? photo : require(`${photo}`)} style={{ maxWidth: '200px' }} />
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+
+                    <Modal.Body><img className=" card-img" src={(photo.includes('64') || photo.includes('blob')) ? photo : require(`${photo}`)} /></Modal.Body>
+                </Modal>
+
                 {text}
                 <h6 className="text-muted" style={{ "justifySelf": 'right' }}>{time}</h6>
             </div>
--- src/Pages/homepage/PhotoViewModal.js
+++ src/Pages/homepage/PhotoViewModal.js
@@ -5,14 +5,14 @@ export default function PhotoViewModal(props) {
         const [show, setShow] = React.useState(props.show);
         const handleClose = () => setShow(false);
         const photo = props.photo;
-      
+
         return (
           <>
             <Modal show={show} onHide={handleClose} centered >
               <Modal.Header closeButton style={{border:'none'}}>
               </Modal.Header>
               <Modal.Body >
-              <img src={(photo.includes('64') || photo.includes('blob'))? photo : require(`${photo}`)} 
+              <img className="card-img" src={(photo.includes('64') || photo.includes('blob'))? photo : require(`${photo}`)}
                 />
               </Modal.Body>
             </Modal>
