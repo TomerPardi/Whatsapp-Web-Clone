7727fd87eb5518a78553ce04a8f2ada3b58c4953
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2022-04-15 12:25:16 +0300

    Added option to share location

--- src/Pages/MessageInput.js
+++ src/Pages/MessageInput.js
@@ -61,6 +61,9 @@ export default function MessageInput(props) {
                     </div>
                 )
                 break
+            case '6':
+                handleLoc();
+                break
             default: return
         }
     }
@@ -111,6 +114,27 @@ export default function MessageInput(props) {
         props.setter(true)
     }
 
+    function handleLoc() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position)=>{
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude
+            const url = 
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+ff0000(${long},${lat})/${long},${lat},15.01,0/200x200@2x?access_token=pk.eyJ1IjoiZnVua2V5N2RhbiIsImEiOiJjbDIwNm13enMwN2tkM3BxNDZjNGRkNDF2In0.DOuTKK-wNrsod_08yK1LWA`;
+            const linkGoogle = `https://www.google.com/maps/search/?api=1&query=${lat}%2C${long}`
+            messages.push({ 'isSelf': true, 'time': new Date().toTimeString().split(' ')[0].slice(0, -3), 'loc': url, type: 'location','linkGoogle':linkGoogle });
+            counterMessages.push({ 'isSelf': false, 'time': new Date().toTimeString().split(' ')[0].slice(0, -3), 'loc': url, type: 'location','linkGoogle':linkGoogle });
+            props.setter(true)
+            }
+            );
+            props.setter(true)
+        } else {
+            alert("Geolocation is not supported by this browser.") ;
+            return
+        }
+
+    }
+
     function handleVideo(videoPath) {
         messages.push({ 'isSelf': true, 'time': new Date().toTimeString().split(' ')[0].slice(0, -3), 'video': videoPath, type: 'video' })
         counterMessages.push({ 'isSelf': false, 'time': new Date().toTimeString().split(' ')[0].slice(0, -3), 'video': videoPath, type: 'video' })
@@ -136,6 +160,14 @@ export default function MessageInput(props) {
                             </svg>}
                             onSelect={handleSelect}
                             style={{ width: '0rem' }}>
+
+                            {/* geolocation button*/}
+                            <Dropdown.Item eventKey="6">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                                </svg>
+                            </Dropdown.Item>
+
                             {/* audio record button*/}
                             <Dropdown.Item eventKey="3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-mic-fill" viewBox="0 0 16 16">
--- src/Pages/homepage/ChatBubble.js
+++ src/Pages/homepage/ChatBubble.js
@@ -4,7 +4,7 @@ import { Modal, Button } from 'react-bootstrap'
 
 
 export default function ChatBubble(props) {
-    const { text, time, isSelf, type, photo, audio,video } = props.message;
+    const { text, time, isSelf, type, photo, audio, video, loc,linkGoogle } = props.message;
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -35,11 +35,11 @@ export default function ChatBubble(props) {
             <div
                 className={`chat-bubble ${isSelf ? "me" : "you"} `}>
                 <Button variant="link" className=' shadow-none' onClick={handleShow}>
-                    <video src={video} style={{ maxWidth: '200px' }} controls autoPlay/>
+                    <video src={video} style={{ maxWidth: '200px' }} controls autoPlay />
                 </Button>
 
                 <Modal show={show} onHide={handleClose}>
-                    <Modal.Body><video className=" card-img" src={video} controls/></Modal.Body>
+                    <Modal.Body><video className=" card-img" src={video} controls /></Modal.Body>
                 </Modal>
 
                 {text}
@@ -52,6 +52,20 @@ export default function ChatBubble(props) {
             <div className={`chat-bubble ${isSelf ? "me" : "you"} player`}>
                 <audio controls src={audio} id='player' style={{ maxHeight: '50px' }}></audio>
                 {text}
+                <h6 className="text-muted" style={{ "justifySelf": 'right' }}>{time}</h6>
+            </div>
+        )
+    }
+
+    else if (type === 'location') {
+        return (
+            <div className={`chat-bubble ${isSelf ? "me" : "you"} player`}>
+                <a href={linkGoogle}  target="_blank">
+                    {console.log(linkGoogle)}
+                    <img src={loc} style={{ maxWidth: '200px' }} >
+                    </img>
+                </a>
+
                 <h6 className="text-muted" style={{ "justifySelf": 'right' }}>{time}</h6>
             </div>
         )
